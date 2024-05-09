d0375a7ec41efb2d6308d8a3ab3621e655bd28cd
ly-zl <saurabhvaidya119@gmail.com>
2024-05-09 17:28:06 +0530

    update  signal

--- vendor/signals.py
+++ vendor/signals.py
@@ -13,14 +13,20 @@ def update_vendor_performance(sender, instance, **kwargs):
 
     # Update On-Time Delivery Rate
     completed_orders = PurchesOrder.objects.filter(vendor=instance.vendor, status='completed')
-    on_time_deliveries = completed_orders.filter( delivery_date__gte=F('delivery_date'))
+    on_time_deliveries = completed_orders.filter(delivery_date__gte=F('delivery_date'))
     on_time_delivery_rate = on_time_deliveries.count() / completed_orders.count()
-    instance.vendor.on_time_delivery_rate = on_time_delivery_rate if on_time_delivery_rate else 0
-
+    if on_time_delivery_rate:
+        instance.vendor.on_time_delivery_rate = on_time_delivery_rate
+    else:
+        instance.vendor.on_time_delivery_rate = 0
+    instance.vendor.save()
     # Update Quality Rating Average
     completed_orders_with_rating = completed_orders.exclude(quality_rating__isnull=True)
     quality_rating_avg = completed_orders_with_rating.aggregate(Avg('quality_rating'))['quality_rating__avg'] or 0
-    instance.vendor.quality_rating_avg = quality_rating_avg if quality_rating_avg else 0
+    if quality_rating_avg:
+        instance.vendor.quality_rating_avg = quality_rating_avg
+    else:
+        instance.vendor.quality_rating_avg = 0
     instance.vendor.save()
 
 @receiver(post_save, sender=PurchesOrder)
@@ -28,7 +34,7 @@ def update_response_time(sender, instance, **kwargs):
     # if instance.acknowledgment_date:
         # Update Average Response Time
         response_times = PurchesOrder.objects.filter(vendor=instance.vendor, acknowledgment_date__isnull=False).values_list('acknowledgment_date', 'issue_date')
-        average_response_time = sum((ack_date - issue_date ).total_seconds() for ack_date, issue_date in response_times)   #/ len(response_times)
+        average_response_time = sum((acknowledgment_date - issue_date ).total_seconds() for acknowledgment_date, issue_date in response_times)   #/ len(response_times)
         if average_response_time < 0:
             average_response_time = 0
         if response_times:
